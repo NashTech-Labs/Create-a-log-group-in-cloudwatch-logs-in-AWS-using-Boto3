b43a89cb8ef35ef4d2e4182c321bc5d71c66d530
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 00:27:07 +0530

    Update in code

--- create_cloud_watch_log_group.py
+++ create_cloud_watch_log_group.py
@@ -1,28 +1,29 @@
-import boto3
 import json
+import boto3
 
-AWS_REGION = input("Enter the AWS_REGION Name")
+REGION = input("Enter the AWS REGION Name: ")
 
-client = boto3.client('logs', region_name=AWS_REGION)
-retention_period_in_days = 5
+log_client = boto3.client('logs', region_name=REGION)
+# these are the valid values are: [1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 2192, 2557, 2922, 3288, 3653] for retention period
+retention_period_in_days = 7
 
 #This configration will use for Backend Log Group.
 
-log_group = input("Enter the name for  backend group")
-response = client.create_log_group(
-    logGroupName=log_group,
+group_name = input("Enter the name for  backend group: ")
+res = log_client.create_log_group(
+    logGroupName=group_name,
     tags={
-        'Type': 'Back end',
-        'Frequency': '30 seconds',
-        'Environment': 'Production',
+        'Type': 'Back_end',
+        'Frequency': '10 seconds',
+        'Environment': 'Prod',
         'RetentionPeriod': str(retention_period_in_days)
     }
 )
 
-print(json.dumps(response, indent=4))
+print(json.dumps(res, indent=4))
 
 response = client.put_retention_policy(
-          logGroupName=log_group,
+          logGroupName=group_name,
           retentionInDays=retention_period_in_days
 )
 
@@ -30,18 +31,19 @@ print(json.dumps(response, indent=4))
 
 #This configration will use for Frontend Log Group.
 
-log_group = input("Enter the name for  Front group")
+log_grp = input("Enter the name for  Front group: ")
 response = client.create_log_group(
-    logGroupName=log_group,
+    logGroupName=log_grp,
     tags={
-        'Type': 'Front end',
-        'Frequency': '30 seconds',
-        'Environment': 'Production',
+        'Type': 'Front_end',
+        'Frequency': '10 seconds',
+        'Environment': 'Prod',
         'RetentionPeriod': str(retention_period_in_days)
     }
 )
 
-response = client.put_retention_policy(
-          logGroupName=log_group,
+res = client.put_retention_policy(
+          logGroupName=log_grp,
           retentionInDays=retention_period_in_days
 )
+print(json.dumps(res, indent=4))
